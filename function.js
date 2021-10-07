47112a5295764a55eb074e705a19db1b942bda15
EBOBO MAXWELL OGHENEMAGA <ebobomaxwwell25@gmail.com>
2021-10-07 09:15:20 +0100

    (function.js)

---
+++ function.js
@@ -0,0 +1,23 @@
+//using objects
+let person1 = {
+    name: "maxwell",
+    age: 23,
+    color: "black",
+}
+// person1.name = "james";
+person1["age"] = "22"
+// person1.car = "benz";
+person1.car = ["benz", "toyota", "volvo"];
+person1["height"] = "12ft";
+console.log(person1);
+
+//using arrays
+let person2 = [
+    names = ["yemi", "victor", "amadin"],
+    age = [23, 32, 44],
+    color = ["yellow", "black", "blue"],
+];
+person2["2"].push("black");
+person2["3"] = ["benz", "toyota"],
+person2["3"].push("volvo", "camry", "tesla", "mitsubish");
+console.log(person2);
