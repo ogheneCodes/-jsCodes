c47765023750de358d1bbec5224365cadc7ae3b9
EBOBO MAXWELL OGHENEMAGA <ebobomaxwwell25@gmail.com>
2021-10-09 17:00:54 +0100

    usinng a conditional statement

---
+++ condition.js
@@ -0,0 +1,19 @@
+//using if statement
+const age = 25;
+if( age > 35){
+    console.log(`my age is ${age} and it is above 30`);
+} else{
+    console.log(`my age is ${age} and it is not above 30`);
+}
+//using elseif statement
+let numb = NaN;
+numb = "hello";
+if(numb === 50){
+console.log(`numb is ${numb} and it is equal to 50`);
+}else if(numb>50){
+    console.log(`numb is ${numb} and it is greater than 50`);
+}else if(numb<50){
+    console.log(`numb is ${numb} and it is less than 50`);
+}else{
+    console.log(`numb is ${numb} and it is nothing`);
+}
