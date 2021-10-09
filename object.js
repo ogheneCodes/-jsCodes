4679466461e05ebc8c8da2be65b303e9559c31a5
EBOBO MAXWELL OGHENEMAGA <ebobomaxwwell25@gmail.com>
2021-10-09 10:59:44 +0100

    changes are made to input arrays and object

--- object.js
+++ object.js
@@ -1,39 +1,64 @@
-//using objects
-let person1 = {
-    name: "maxwell",
-    age: 23,
-    color: "black",
-}
-// person1.name = "james";
-person1["age"] = "22"
-// person1.car = "benz";
-person1.car = ["benz", "toyota", "volvo"];
-person1["height"] = "12ft";
-console.log(person1);
-
-//using arrays
-let person2 = [
-    names = ["yemi", "victor", "amadin"],
-    age = [23, 32, 44],
-    color = ["yellow", "black", "blue"],
+//using an Array to declare variables
+const person1 = [
+    property = ["car", "furniture", "TV"],
+    cars = ["toyota", "forbs"],
+    wives = ["shade", "tonia", "rubish"],
 ];
-person2["2"].push("black");
-person2["3"] = ["benz", "toyota"],
-// person2["3"].push("volvo");
-person2["3"].unshift("camry");
-person2["3"].unshift("ferarri");
-person2["2"].unshift("orange");
-console.log(person2);
-// console.dir("Maxwell is good");
+// console.log(person1[1].length);
+//using an object to declare variales(only collect's one value for each property
+// unless you will us object inside object)
+const person2 = {
+    property: "house", cars: "volvos", wife:"shola",
+};
+// console.log(person2["cars"].length);
+//using arrays inside an object
+// const person3 = {
+//     property = ["house", "cars", "chairs", "Tables", "TV"],
+//     cars = ["ferari", "BMW", "toyota"],
+//     wives = ["tolu", "teni", "shuwa"],
+// }
+// console.log(typeof(person3));
 
-const firstName = "Ebobo";
-const lastName = "Maxwell";
-const age = 23;
-const color = "black";
-const career = [
-    academic = ["Librarian", "Teacher", "Writer", "Editor", "researcher"],
-    IT = ["web_dev", "cloud_comp", "Data_Sci", "Mobile_dev", "Digital_Mkt"],
-    office = ["remote", "physical"]
+//using objects inside an array
+const person4 = [
+    person_a = {
+        property: "house", cars: "toyota", wife: "tutu",
+    },
+    person_b = {
+        property: "house11", cars: "Benz", wife: "titi",
+    },
+    person_c = {
+        property: "knife", cars: "ferari", wife: "shudira",
+    },
+    person_d = {
+        property: "kettle", cars: "lorry", wife: "monica",
+    },
 ];
-console.log(` this are my details: ${firstName}, ${lastName}, ${age},${color}, ${career}`);
-// console.log(`my name is ${firstName} ${lastName} and i am ${age}years old`);
+console.log(person4[1]);
+// console.log(person4[1].property.length);
+//using objects inside an object
+// const person5 = {
+//     person_a = {
+//         property: "chair", cloths: 3, cars: "Benz", wife: "amazin",
+//     },
+//     person_b = {
+//         property: "totle", cloths: 4, cars: "camry", wife: "zozo",
+//     },
+//     person_c = {
+//         property: "kitta", cars: "ferari22", wife: "shudirawa",
+//     },
+//     person_d = {
+//         property: "kettle122", cars: "lorrwwo", wife: "monicana",
+//     },
+// }
+// // console.log(person5);
+
+//using the push method
+
+//using the unshift method
+
+//push skills
+
+//using loop for in arrays for lenght in arrays
+
+// using loop for object for length in objects
